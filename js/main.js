50777df388293c2190f30653ab161e1060534be8
Jerry Chua <jerry@tachy.sg>
2018-05-20 16:48:06 +0800

    Menu Button Part 2

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@
 	$(".menu-button").click(function(){
 		$(".menu-wrapper, .menu-button").toggleClass("on");
 	});
+	$(".dropdown").click(function(){
+		$(this).find(".dd-menu").toggle();
+		$(this).find(".dd-arrow").toggleClass("on");
+	})
 })(jQuery)
